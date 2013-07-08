ee5a2936b62ab17f7358e5ca186df12ce39e351d
fabien0102 <fabien0102@planee.fr>
2013-07-08 14:55:09 +0200

    Add init method on the main

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var NoMoPo = exports;
+var nomoco = exports;
 
 exports.Hexy  = require('./lib/Hexy');
 exports.Servo = require('./lib/Servo');
+
+exports.init = function(options) {
+    var hexy = new nomoco.Hexy(options);
+    
+    return hexy;
+};
