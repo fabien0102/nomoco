7b84fa12f20ee23d96fb431d79c32b580621b988
fabien0102 <fabien0102@planee.fr>
2013-07-23 19:05:03 +0200

    Walk example

--- example/base.js
+++
@@ -1,6 +0,0 @@
-var hexy = require('../')();
-
-hexy.on('ready', function(){
-  // Some instructions...
-  hexy.LF.ankle.position = 30;
-});
---
+++ example/walk.js
@@ -0,0 +1,36 @@
+var hexy = require('../')();
+var async = require('async');
+
+var deg = 25
+var midFloor = 30
+var hipSwing = 25
+var pause = 0.5
+
+hexy.on('ready', function(){
+  // Some instructions...
+  async.timesSeries(5, function (n, next){
+    // 1st step
+    hexy.LF.replantFoot(deg-hipSwing,stepTime=0.5);
+    hexy.RM.replantFoot(hipSwing,stepTime=0.5);
+    hexy.LB.replantFoot(-deg-hipSwing,stepTime=0.5);
+
+    hexy.RF.hip.position = -deg-hipSwing;
+    hexy.LM.hip.position = hipSwing;
+    hexy.RB.hip.position = deg-hipSwing;
+    
+    // 2nd step
+    setTimeout(function(){
+      hexy.RF.replantFoot(-deg+hipSwing,stepTime=0.5);
+      hexy.LM.replantFoot(-hipSwing,stepTime=0.5);
+      hexy.RB.replantFoot(deg+hipSwing,stepTime=0.5);
+  
+      hexy.LF.hip.position = deg+hipSwing;
+      hexy.RM.hip.position = -hipSwing;
+      hexy.LB.hip.position = -deg+hipSwing;
+      next();
+    }, 600);
+  
+  }, function(){
+    console.log('Finish !!!');
+  })
+});
